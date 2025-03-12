f46f442e9d4ff7975b51d85ff5d6f94a87edbe78
Budi Prasetya <bpchen@gmail.com>
2025-03-12 15:17:22 -0700

    Remove default database to postgres15

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
