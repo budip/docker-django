e88b4c2fae66b92aa67b0b17fd0a69ab1347c936
Budi Prasetya <bpchen@gmail.com>
2025-03-13 13:31:23 -0700

    add nasa apod

--- backend/apps/nasa/views.py
+++ backend/apps/nasa/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.conf import settings
+from django.shortcuts import render
 import requests
+import json
 
 def index(request):
     return JsonResponse({"message": "Welcome to the NASA API!"})
@@ -11,11 +13,11 @@ def get_nasa_data():
 
 def apod(request):
     api_url = "https://api.nasa.gov/planetary/apod"
-    params = {
-        "api_key": settings.NASA_API_KEY
-    }
+    params = {"api_key": settings.NASA_API_KEY}
     
     response = requests.get(api_url, params=params)
     data = response.json()
-    
-    return JsonResponse(data)
+
+    formatted_json = json.dumps(data, indent=2)
+
+    return render(request, "nasa/apod.html", {"data": data, "formatted_json": formatted_json})
